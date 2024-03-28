63e2b28ca1e682d42fb8aa9b84ff112052060125
Ivanna Navrotska <navrockaivanna15@gmail.com>
2024-03-28 23:12:57 +0200

    Commit for master

--- my_python/fibonacci.py
+++ my_python/fibonacci.py
@@ -4,9 +4,7 @@ def fibonacci (f):
     else:
         return fibonacci(f-1) + fibonacci(f-2)
     
-print(fibonacci(5))
 print('this is for master')
-print(fibonacci(4+6))
-print('this is for dev')
-print(fibonacci(9-7))
+print(fibonacci(5))
+
 
